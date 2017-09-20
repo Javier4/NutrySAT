97e6502f80c6d7ebbc42dde0e017b9b1447ffd3e
NUTRYTECSPORT\webmaster4 <webmaster4@gruponutrytec.com>
2017-09-20 10:03:57 +0200

    Agregado ReaperTheme

--- NutrySAT/NutrySAT/Form1.cs
+++ NutrySAT/NutrySAT/Form1.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace NutrySAT
 {
-    public partial class Form1 : Form
+    public partial class Form1 : ReaperTheme.ReaperForm
     {
         public Form1()
         {
